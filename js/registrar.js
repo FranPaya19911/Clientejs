2ed153368b5286b5a0c58f3816890a4d67f78b22
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-17 15:27:39 +0100

    tecladoregis

--- js/registrar.js
+++ js/registrar.js
@@ -2,20 +2,28 @@ function validador() {
     var nombre = document.getElementById("nombre").value;
     var apellidos = document.getElementById("apellidos").value;
     var email = document.getElementById("email").value;
+    var telefono = document.getElementById("telefono").value;
     var password = document.getElementById("pass1").value;
     var password2 = document.getElementById("pass2").value;
     var imagen = document.getElementById("img").src;
+    var correcto = true;
+
+    correcto = false;
+    if (correcto) {
+        escribir(nombre, apellidos, email, telefono, password, imagen);
+        location.href = "Iniciar.html";
+    } else {
+        alert("error");
+    }
 
 
-    escribir(nombre, apellidos, email, password, imagen);
 
-    location.href = "Iniciar.html";
 
 }
-
+//validar por teclado el telefono
 function teclatelefono(event) {
     var evento = event || window.event;
-    var code = evento.keyCode;
+    var code = evento.charcode;
     if (((code >= 48) && (code <= 57)) || ((code >= 96) && (code <= 105)) || (code == 8) || (code == 37) || (code == 39) || (code == 46)) {
         return true;
     } else {
@@ -23,10 +31,11 @@ function teclatelefono(event) {
     }
 }
 
-function escribir(user, apellidos, email, pass, img) {
+function escribir(user, apellidos, email, telefono, pass, img) {
     document.cookie = "Nombre_" + user + "=" + user;
     document.cookie = "apellidos" + user + "=" + apellidos;
     document.cookie = "email_" + user + "=" + email;
+    document.cookie = "telefono" + user + "=" + telefono;
     document.cookie = "pass_" + user + "=" + pass;
     document.cookie = "img_" + user + "=" + img;
 }
