e62068571bf5e303e65e98eca657b28d28462d83
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-24 16:20:03 +0100

    od

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,8 @@
 function entrar() {
+    var correo = document.getElementById("email").value;
+    var passwd = document.getElementById("password").value;
 
+    if (correo == "admin@admin.com" && passwd == "Admin1234") {
+        location.href = "Admin.html";
+    }
 }
